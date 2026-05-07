f76773e869a31afebb55e4f7712a2611ef9258c9
Pardha05 <pardhasaibobba@gmail.com>
2026-05-07 22:24:19 +0530

    Downgrade to Express 4 to fix wildcard routing issues and restore stability

--- server.cjs
+++ server.cjs
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/:path(.*)', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 const server = http.createServer(app);
